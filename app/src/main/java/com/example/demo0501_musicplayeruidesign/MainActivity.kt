59033c8821b019dd42f8822b590f825266f3b7d0
xiaoxiong1006 <758063761@qq.com>
2020-08-24 17:59:56 +0800

    Initial Commit:add customize styles and themes

---
+++ app/src/main/java/com/example/demo0501_musicplayeruidesign/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demo0501_musicplayeruidesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
